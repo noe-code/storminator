fdd601a1f955f42a9282800f8368797f7ced5db8
noe-code <elmaildenoe@gmail.com>
2014-06-03 17:01:48 +0200

    Add dash and about subviews

--- js/app/views/app.js
+++ js/app/views/app.js
@@ -2,9 +2,10 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'app/views/counter'
+  'app/views/dash'
+  'app/views/about'
 
-  ], function ($, _, Backbone, CounterView) {
+  ], function ($, _, Backbone, DashView, Aboutview) {
       'use strict';
 
       var AppView = Backbone.View.extend({
@@ -14,38 +15,34 @@ define([
             '<div class="navbar">',
               '<a class="navbar-brand" href="#">Storminator</a>',
               '<ul class="nav navbar-nav">',
-                  '<li id="nav-dash"><a href="#">Dashboard</a></li>',
-                  '<li id="nav-about"><a href="#">About</a></li>',
+                  '<li id="nav-dash"><a href="#dash">Dashboard</a></li>',
+                  '<li id="nav-about"><a href="#about">About</a></li>',
               '</ul>',
             '</div>',
             '<div id="content"></div>'
         ].join(''),
 
           events: {
-            'click #nav-dash': 'onNavDash',
-            'click #nav-about': 'onNavAbout'
+
           },
 
         views: {},
 
         initialize: function() {
-          this.views['counter'] = new CounterView({
-            id: 'counter-widget',
-            className: 'counter-widget'
+          this.views['dash'] = new DashView({
+            id: 'page-dash',
+            className: 'page-view'
+          });
+
+          this.views['about'] = new AboutView({
+            id: 'page-about',
+            className: 'page-view'
           });
 
           this.$el.append(this.html);
-          this.$('#content').append(this.views['counter'].render().el);
-        },
-
-        onNavDash: function(){
-          alert('Dashboard button cliked');
-        },
-
-        onNavAbout: function(){
-          alert('About button cliked');
+          this.$('#content').append(this.views['dash'].render().el);
+          this.$('#content').append(this.views['about'].render().el);
         }
-
       });
 
       return AppView;
