2117492a4cefd1fbff605fdb6a5ae2118cc99ab4
noe-code <elmaildenoe@gmail.com>
2014-06-07 18:12:56 +0200

    Convert celsius to fahrenheit test RED status

---
+++ js/app/utils.js
@@ -0,0 +1,9 @@
+define([], function(){
+  'use strict';
+
+  var toFahrenheit = function(celsius) {
+    return 0
+  };
+
+  return {toFahrenheit: toFahrenheit}
+});
--- js/tests/utilsTest.js
+++ js/tests/utilsTest.js
@@ -1,5 +1,15 @@
 define([
-  
-], function(){
+  'app/utils'
+], function(Utils){
   'use strict';
+
+  var run = function() {
+    test('converts celsius to fahrenheit', function(){
+      strictEqual(Utils.toFahrenheit(32),89.6,'converts 32c to 89.6F');
+      strictEqual(Utils.toFahrenheit("hot"), null, 'returns null when given a string');
+      strictEqual(Utils.toFahrenheit(), null, 'returns null when given undefined')
+    });
+  };
+
+  return {run: run}
 });
